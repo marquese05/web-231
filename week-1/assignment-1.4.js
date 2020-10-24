765ea879915c68d05488b7016f858e9b83b1c4a9
marquese05 <marpowell1@my365.bellevue.edu>
2020-10-24 15:00:08 -0500

    Adding Assignment 1.4

---
+++ week-1/assignment-1.4.js
@@ -0,0 +1,17 @@
+var firstName = "Marcus";
+
+console.log(firstName);
+
+var lastName = "Powell";
+
+console.log(lastName);
+
+var age = "35";
+var street = "222 West State";
+var city = "Chicago";
+var zipCode = "60664";
+
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
