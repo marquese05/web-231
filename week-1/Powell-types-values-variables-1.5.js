186e78dc6752d2795c9ae251e3e9e35ac1bbc875
marquese05 <marpowell1@my365.bellevue.edu>
2020-10-24 16:04:02 -0500

    Completed 1.5

---
+++ week-1/Powell-types-values-variables-1.5.js
@@ -0,0 +1,55 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Marcus Powell
+; Date: 10/23/2020
+; Modified By: NA
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/
+
+var firstName1 = "Marcus";
+var firstName2 = "John";
+var firstName3 = "Rob";
+
+var lastName1 = "Carter";
+var lastName2 = "Jones";
+var lastName3 = "Smith";
+
+var address1 = "222 Spooner";
+var address2 = "333 Rockwell";
+var address3 = "121 Parker";
+
+var payRate1 = 7.8955;
+var payRate2 = 9.4584;
+var payRate3 = 6.5822;
+
+var hireDate1 = new Date().toDateString('en-US'); //date is set to current date
+var hireDate2 = new Date().toDateString('en-US'); //date is set to current date
+var hireDate3 = new Date().toDateString('en-US'); //date is set to current date
+
+
+console.log(firstName1);
+console.log(lastName1);
+console.log(address1);
+console.log(payRate1.toFixed(1)); //Fixing Payrate to 1 decimal
+console.log(hireDate1);
+
+console.log(firstName2);
+console.log(lastName2);
+console.log(address2);
+console.log(payRate2.toFixed(1)); //Fixing Payrate to 1 decimal
+console.log(hireDate2);
+
+console.log(firstName3);
+console.log(lastName3);
+console.log(address3);
+console.log(payRate3.toFixed(1)); //Fixing Payrate to 1 decimal
+console.log(hireDate3);
+
+
+
+
+
