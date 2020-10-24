110706ca0fdec26fa64dc0e192c3f5e43d7cf1ee
marquese05 <marpowell1@my365.bellevue.edu>
2020-10-24 15:11:17 -0500

    Name Update

---
+++ week-1/Powell-primitivetypes-1.4.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Marcus Powell
+; Date: 10/23/2020
+; Modified By: NA
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/
+
+var firstName = "Marcus";
+
+console.log(firstName);
+
+var lastName = "Powell";
+
+console.log(lastName);
+
+var age = "35";
+var street = "222 West State";
+var city = "Chicago";
+var zipCode = "60664";
+
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
