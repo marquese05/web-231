08c42f999dc34f050fe6ef62b9be23bc731cdab4
marquese05 <marpowell1@my365.bellevue.edu>
2020-10-25 21:20:22 -0500

    DQ1.1

---
+++ week-1/Powell-sp-1.1.js
@@ -0,0 +1,33 @@
+/* 
+
+============================================ 
+
+; Title:  Discussion 1.1 
+
+; Author: Georgia Welch Cronin and ideas from Richard Krasso  
+
+; Modified by: Georgia Welch Cronin 
+
+; Date:  21 October 2020 
+
+; Description: Creating a simple Javascript example that uses values and variables and has at three small errors that can be fixed. 
+
+;=========================================== 
+
+*/ 
+let firstActivity = 'running,'; 
+
+let secondActivity = 'cycling,'; 
+
+let thirdActivity = 'hiking,'; 
+
+let fourthActivity = 'camping.'; 
+
+ 
+
+//the output of the list above 
+
+console.log('My favorite activities are ' + firstActivity + '' + secondActivity + '' +  thirdActivity + 'and ' +  fourthActivity ); 
+
+/*Explanation fixed the firstActivity by capitalizing the A. Also removed spacing between apostrophie. Added plus before secondActivity. 
+Lastly added space after the word "are".*/
