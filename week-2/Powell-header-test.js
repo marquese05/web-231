a9f72a23763ee43ca0cdf84ee2559abf8cc35b24
marquese05 <marpowell1@my365.bellevue.edu>
2020-11-01 20:15:52 -0600

    Powell Header Test

---
+++ week-2/Powell-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  Powell-header-test.js
+; Author: Professor Krasso 
+; Modified By: Marcus Powell
+; Date:   11/1/2020
+; Description: Displays a formatted header
+;===========================================
+*/
+const header = require('../header.js');
+
+console.log(header.display("Marcus", "Powell", "Exercise 2.2 - Header"));
