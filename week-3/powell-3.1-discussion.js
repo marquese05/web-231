04849bb0393a12383c2564be305e062424447d80
marquese05 <marpowell1@my365.bellevue.edu>
2020-11-05 18:43:09 -0600

    3.1 DQ Added

---
+++ week-3/powell-3.1-discussion.js
@@ -0,0 +1,23 @@
+/* ============================================
+; Title: Assignment 3.1
+; Author: MDN web docs (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#while_statement)
+; Date: 5 November 2020
+; Modified By: Marcus Powell
+; Description: This program demonstrates the use of JavaScript while loop statements in an application to produce addition and multiplication. It will have two errors within the statement.
+;=========================================== */
+
+//Declare variables
+
+let z = 0;
+let x = 0;
+
+//While statement
+
+while (z < 3) {
+  z+;
+  x += z;
+}
+
+console.log(x+z); //Adding variables to loop until it terminates. Sum should equal 9
+
+console.log(x*b); //Multiply variables to loop until it terminates. Product should equal 18
