9827f091fa2b9484d05cbf6694f6620173706fbc
marquese05 <marpowell1@my365.bellevue.edu>
2020-11-05 19:11:33 -0600

    DQ response to Ethan

---
+++ week-3/ethan-dq reply.js
@@ -0,0 +1,20 @@
+/**
+ * Title: Discussion 3.1
+ * Author: Ethan Kenneth Winter
+ * Date: 5 November 2020
+ * Modified by: Marcus Powell
+ * Description: Create a simple JavaScript program using either a for, while, if, or switch statement with at least two errors.
+ */
+
+//let header = require('../week-2/winter-header.js'); (Unlinked from directory)
+
+//console.log(header.display("Ethan Kenneth", "Winter", "Discussion 3.1 Control Statements"));
+
+
+let seven = 7;
+let eight = 8; //Removed commas from numeral for consistency
+
+//if seven is less than eight make a note in the console (Fixed mis-spelling on console)
+if (seven > eight) {
+  console.log("Seven is greater than eight!");
+} else console.log("Seven is not greater than eight!"); //added else statement for true sentence.
